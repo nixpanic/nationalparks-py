b1ba0999a0c3be5a557c6f1f2b85e43d2521aa63
Niels de Vos <ndevos@redhat.com>
2017-07-24 13:02:06 +0200

    wsgi: change displayName
    This change should notify the deployment through a WebHook.

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'National Parks (PY)',
+        'displayName': 'National Parks (on OpenShift from GitHub/nixpanic)',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
